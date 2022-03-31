ecea5c3749f7c6ff6dacc8f11c82f346dd77dae8
fatihege <ifatxh@gmail.com>
2022-03-31 18:42:07 +0300

    made some refactoring in javascript code

--- js/main.js
+++ js/main.js
@@ -11,9 +11,6 @@ const faviconImages = {
     light: 'https://fatihege.github.io/cps-test/img/favicon-light_128x128.png',
 }
 
-highCPSElem.innerText = (localStorage.getItem('high_cps') || 0).toString();
-highScoreElem.innerText = (localStorage.getItem('high_score') || 0).toString();
-
 const changeTheme = () => {
     if (localStorage.getItem('dark_mode') === '1') {
         document.body.classList.add('dark');
@@ -23,8 +20,7 @@ const changeTheme = () => {
                     <path d="M241.774,468.88C241.774,476.732 248.148,483.106 256,483.106C263.852,483.106 270.226,476.732 270.226,468.88L270.226,403C270.226,395.148 263.852,388.774 256,388.774C248.148,388.774 241.774,395.148 241.774,403L241.774,468.88ZM95.411,396.47C89.86,402.022 89.86,411.037 95.411,416.589C100.963,422.14 109.978,422.14 115.53,416.589L162.115,370.004C167.667,364.452 167.667,355.437 162.115,349.885C156.563,344.333 147.548,344.333 141.996,349.885L95.411,396.47ZM396.47,416.589C402.022,422.14 411.037,422.14 416.589,416.589C422.14,411.037 422.14,402.022 416.589,396.47L370.004,349.885C364.452,344.333 355.437,344.333 349.885,349.885C344.333,355.437 344.333,364.452 349.885,370.004L396.47,416.589ZM256,140.117C319.958,140.117 371.883,192.042 371.883,256C371.883,319.958 319.958,371.883 256,371.883C192.042,371.883 140.117,319.958 140.117,256C140.117,192.042 192.042,140.117 256,140.117ZM43.12,241.774C35.268,241.774 28.894,248.148 28.894,256C28.894,263.852 35.268,270.226 43.12,270.226L109,270.226C116.852,270.226 123.226,263.852 123.226,256C123.226,248.148 116.852,241.774 109,241.774L43.12,241.774ZM468.88,270.226C476.732,270.226 483.106,263.852 483.106,256C483.106,248.148 476.732,241.774 468.88,241.774L403,241.774C395.148,241.774 388.774,248.148 388.774,256C388.774,263.852 395.148,270.226 403,270.226L468.88,270.226ZM416.589,115.53C422.14,109.978 422.14,100.963 416.589,95.411C411.037,89.86 402.022,89.86 396.47,95.411L349.885,141.996C344.333,147.548 344.333,156.563 349.885,162.115C355.437,167.667 364.452,167.667 370.004,162.115L416.589,115.53ZM115.53,95.411C109.978,89.86 100.963,89.86 95.411,95.411C89.86,100.963 89.86,109.978 95.411,115.53L141.996,162.115C147.548,167.667 156.563,167.667 162.115,162.115C167.667,156.563 167.667,147.548 162.115,141.996L115.53,95.411ZM270.226,43.12C270.226,35.268 263.852,28.894 256,28.894C248.148,28.894 241.774,35.268 241.774,43.12L241.774,109C241.774,116.852 248.148,123.226 256,123.226C263.852,123.226 270.226,116.852 270.226,109L270.226,43.12Z"/>
                 </g>
             </svg>`;
-    }
-    else {
+    } else {
         document.body.classList.remove('dark');
         toggleThemeBtn.innerHTML = `
             <svg width="100%" height="100%" viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
@@ -87,6 +83,7 @@ const setFavicon = () => {
 
 const init = () => {
     setFavicon();
+    changeTheme();
     updateScores();
     highCPSElem.innerText = (localStorage.getItem('high_cps') || 0).toString();
     highScoreElem.innerText = (localStorage.getItem('high_score') || 0).toString();
@@ -102,14 +99,14 @@ const init = () => {
     }
     conf.time = conf.initTime;
     let finished = false;
-    changeTheme();
-    window.cpsInterval = null;
+    let cpsInterval = null;
     timerElem.innerText = conf.time;
 
     addEventListener('click', (e) => {
         if (
             (e.target.classList.contains(toggleThemeBtn.classList[0])) ||
-            (e.target.classList.contains(restartBtn.classList[0]))
+            (e.target.classList.contains(restartBtn.classList[0])) ||
+            (e.target.tagName === 'A')
         ) return;
 
         if (!finished) {
@@ -118,8 +115,8 @@ const init = () => {
             scoreElem.innerText = conf.score;
         }
 
-        if (window.cpsInterval === null && !finished) {
-            window.cpsInterval = setInterval(() => {
+        if (cpsInterval === null && !finished) {
+            cpsInterval = setInterval(() => {
                 conf.cps = conf.score / conf.initTime;
                 updateScores(conf);
 
@@ -133,11 +130,11 @@ const init = () => {
                     } else {
                         if (conf.tempScore >= 0) addCPSLog(conf.tempScore);
                         conf.tempScore = -1;
-                        window.cpsInterval = null;
+                        cpsInterval = null;
                         finished = true;
                         timerElem.innerText = 0;
                         restartBtn.parentElement.classList.remove('hidden');
-                        clearInterval(window.cpsInterval);
+                        clearInterval(cpsInterval);
                     }
                 }
 
@@ -149,6 +146,6 @@ const init = () => {
     addEventListener('keydown', (e) => (e.keyCode === 88) ? setTheme() : false);
 }
 
-addEventListener('load', init);
+addEventListener('load', () => init());
 toggleThemeBtn.addEventListener('click', () => setTheme());
 restartBtn.addEventListener('click', () => location.reload());
